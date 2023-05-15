5450cf00ac5da3b596b5c970af45d8126be839b1
zubair6561 <zubairalibutt656@gmail.com>
2023-05-15 10:46:40 +0500

    initial  commit

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,7 @@
+// inside app/javascript/controllers/index.js
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
